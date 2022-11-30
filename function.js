9963db2f9c18ea0917d3f13e7b1f79b71b264d2b
Sai Krisj <118242400+sai7354@users.noreply.github.com>
2022-11-30 13:18:28 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,24 @@
+function sum(x,y){
+    let add =x+y;
+    return add;
+   }
+   function diff(x,y){
+     let sub =x-y;
+     return sub;
+   }
+  function multiplication(x,y){
+    let mul= x*y;
+    return mul;
+  }
+  function divide(x,y){
+    let div =x/y;
+    return div;
+  }
+  let sum1= sum(10,5)
+  console.log(sum1)
+  let sub1= diff(10,5)
+  console.log(sub1)
+  let mul1= multiplication(10,5)
+  console.log(mul1)
+  let div1= divide(10,5)
+  console.log(div1)
